ad62bc1a58a53eeb10a3fbb743c2f5be3829100c
Артем <iron-man1998@mail.ru>
2024-12-21 23:20:35 +0300

    Удаление ненужного класса

--- SpringCompass/src/main/java/ru/appline/controller/Controller.java
+++ SpringCompass/src/main/java/ru/appline/controller/Controller.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.appline.logic.CompassModel;
-
 import java.util.HashMap;
 import java.util.Map;
 
